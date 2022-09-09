b6708fb13b832c21f51621b5fdfc24c6a55b2496
Tuna YAPRAK <tyaprak@gmail.com>
2022-09-09 19:09:41 +0300

    srted

--- data.py
+++ data.py
@@ -1,18 +1,20 @@
 import pymongo
-
 client = pymongo.MongoClient('mongodb://localhost:27017/')
 db = client.abbr
 conn = db.abbr
-
 class Data:
     def __init__(self,abb):
         self.abb=abb.upper()
         self.target = { "abbr":abb }
         self.data=conn.find(self.target)
+        self.tamData=conn.find().sort("abbr",pymongo.ASCENDING)
         self.t10=conn.find().sort("v",pymongo.DESCENDING)
+        self.sorted_abbs=[]
         for i in self.data:
             self.ff=i["fullforms"]
             self.count=i["v"]
+        for i in self.tamData:
+            self.sorted_abbs.append(i["abbr"])
         try:
             if len(self.ff)>0:
                 self.isok=True
